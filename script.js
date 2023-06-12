ca4e3eb4f1fc2f8fb1a18168758b92c1d993711b
yash <yashsaundalkar0@gmail.com>
2023-06-12 10:35:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ async function checkWeather(city){
     else{
         var data = await response.json();
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".temp").innerHTML = data.main.temp + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
