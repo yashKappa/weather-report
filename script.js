57a0dd7d096d5f01544a1dc15c914739a89607a5
yash <yashsaundalkar0@gmail.com>
2023-07-25 21:27:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,25 +20,25 @@ async function checkWeather(city){
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
     if(data.weather[0].main == "Clouds"){
-        weatherIcon.src = "https://myteenwebcam.com/fapp/gifs/9c0bcd2c5922acf7a8131208fbb8f0db.gif";
+        weatherIcon.src = "clouds.png";
     }
     else if(data.weather[0].main == "Rain"){
-        weatherIcon.src ="https://porngifs.xxx/wp-content/uploads/2023/04/dp-blow-porn-gifs-sex-gif.gif";
+        weatherIcon.src ="rain.png";
     }
     else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src ="https://gifcandy.net/wp-content/uploads/2022/11/gifcandy-1.gif";
+        weatherIcon.src ="drizzle.png";
     }
     else if(data.weather[0].main == "Clear"){
-        weatherIcon.src ="https://megapornx.com/xxx/milf-gifs-porn-gifs-1.gif";
+        weatherIcon.src ="clear.png";
     }
     else if(data.weather[0].main == "Mist"){
-        weatherIcon.src ="https://cdn5-images.motherlessmedia.com/images/DD6A3F1.gif";
+        weatherIcon.src ="mist.png";
     }
     else if(data.weather[0].main == "Smoke"){
-        weatherIcon.src ="https://myteenwebcam.com/fapp/gifs/9c0bcd2c5922acf7a8131208fbb8f0db.gif";
+        weatherIcon.src ="clear.png";
     }
     else if(data.weather[0].main == "Snow"){
-        weatherIcon.src ="https://porngifs.xxx/wp-content/uploads/2023/04/dp-blow-porn-gifs-sex-gif.gif";
+        weatherIcon.src ="snow.png";
     }
 
     document.querySelector(".weather").style.display = "block"
