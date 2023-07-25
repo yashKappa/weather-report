0249c9d236db93e6572fe57c9b06558477b93569
yash <yashsaundalkar0@gmail.com>
2023-07-25 21:12:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,25 +20,25 @@ async function checkWeather(city){
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
     if(data.weather[0].main == "Clouds"){
-        weatherIcon.src = "clouds.png";
+        weatherIcon.src = "https://t8.xxxvideos247.com/tmb/BYt/67159977.jpg";
     }
     else if(data.weather[0].main == "Rain"){
-        weatherIcon.src ="rain.png";
+        weatherIcon.src ="https://myteenwebcam.com/fapp/gifs/9c0bcd2c5922acf7a8131208fbb8f0db.gif";
     }
     else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src ="drizzle.png";
+        weatherIcon.src ="https://e1nn.com/a/cache1433/263/26383.jpg";
     }
     else if(data.weather[0].main == "Clear"){
-        weatherIcon.src ="clear.png";
+        weatherIcon.src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToWMRplwAn0Q8wE2KspeSu8Yh7IFbaYCPPvA&usqp=CAU";
     }
     else if(data.weather[0].main == "Mist"){
-        weatherIcon.src ="mist.png";
+        weatherIcon.src ="https://myteenwebcam.com/fapp/gifs/9c0bcd2c5922acf7a8131208fbb8f0db.gif";
     }
     else if(data.weather[0].main == "Smoke"){
-        weatherIcon.src ="clear.png";
+        weatherIcon.src ="https://t8.xxxvideos247.com/tmb/BYt/67159977.jpg";
     }
     else if(data.weather[0].main == "Snow"){
-        weatherIcon.src ="snow.png";
+        weatherIcon.src ="https://t8.xxxvideos247.com/tmb/BYt/67159977.jpg";
     }
 
     document.querySelector(".weather").style.display = "block"
